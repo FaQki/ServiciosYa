8db7ad76af3783c8912502ac56a3c1085e53b5fe
Facundo Angelico <facuang@outlook.com>
2022-06-27 18:13:45 -0300

    Changes for naza

--- mediumish-html/assets/js/mediumish.js
+++ mediumish-html/assets/js/mediumish.js
@@ -1,11 +1,12 @@
-$(function(){
+$(function () {
   var topOfOthDiv = $(".hideshare").offset().top;
-  $(window).scroll(function() {
-      if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
-          $(".share").hide(); //reached the desired point -- show div
-      }
-      else{
-        $(".share").show();
-      }
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > topOfOthDiv) {
+      //scrolled past the other div?
+      $(".share").hide(); //reached the desired point -- show div
+    } else {
+      $(".share").show();
+    }
   });
 });
+
